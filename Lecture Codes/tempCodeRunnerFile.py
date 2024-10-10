76739944344edebcda8936ebc9235be611bd3c73
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-10-10 13:38:17 +0530

    made changes in the files

--- Lecture Codes/tempCodeRunnerFile.py
+++ Lecture Codes/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-        
+from time import sleep
+from threading import Thread
+thread = Thread(target=lambda:sleep(1))
+print(thread.is_alive())
+thread.start()
+print(thread.is_alive())
+thread.join()
+print(thread.is_alive())
---
+++ Lecture Codes/twenty_one.py
@@ -0,0 +1,34 @@
+# from threading import Thread
+# thread = Thread(daemon=True)
+# print(thread.daemon)
+
+
+
+# from threading import Thread
+# thread = Thread()
+# thread.daemon = True
+# print(thread.daemon)
+
+
+# Thread isalive()
+
+# from time import sleep
+# from threading import Thread
+# thread = Thread(target=lambda:sleep(1))
+# print(thread.is_alive())
+# thread.start()
+# print(thread.is_alive())
+# thread.join()
+# print(thread.is_alive())
+
+
+# Thread identifier
+
+# from threading import Thread
+# thread = Thread()
+# print(thread.ident)
+# thread.start()
+# print(thread.ident)
+
+
+

0b56bf72a7b2a706c2e41346407bab3b46ce17fa
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-10-10 13:53:51 +0530

    made changes in the files

--- Lecture Codes/twenty_two.py
+++ Lecture Codes/twenty_two.py
@@ -2,7 +2,7 @@ import smtplib
 smtp_server ='smtp.gmail.com'
 port = 587  #for TLS
 sender_email = 'sumeetkapadia47@gmail.com'
-password ='nigger'
+password ='lmaoded'
 server = smtplib.SMTP(smtp_server,port)
 server.starttls()
 server.login(sender_email,password)
